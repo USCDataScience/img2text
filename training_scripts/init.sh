1f59ddcd7599b8d3b1a451b211b8f97e6d0aab67
Thejan Wijesinghe <thejanwijesinghe.14@cse.mrt.ac.lk>
2018-01-05 03:22:49 +0530

    Avoid creating folders

--- training_scripts/init.sh
+++ training_scripts/init.sh
@@ -18,7 +18,7 @@ mkdir -p "${FLICKR8K_PROCESSED_TEXT_DIR}"
 mkdir -p "${INCEPTION_V3_DIR}"
 
 wget "${INCEPTION_V3_URL}" && tar -xzvf inception_v3_2016_08_28.tar.gz -C "${INCEPTION_V3_DIR}" && rm -rf inception_v3_2016_08_28.tar.gz
-wget "${FLICKR8K_DATASET_URL}" && unzip Flickr8k_Dataset.zip -d "${FLICKR8K_DATASET_DIR}" && rm -rf Flickr8k_Dataset.zip
+wget "${FLICKR8K_DATASET_URL}" && unzip -j Flickr8k_Dataset.zip -d "${FLICKR8K_DATASET_DIR}" && rm -rf Flickr8k_Dataset.zip
 wget "${FLICKR8K_TEXT_URL}" && unzip Flickr8k_text.zip -d "${FLICKR8K_TEXT_DIR}" && rm -rf Flickr8k_text.zip
 
 cd im2txt
